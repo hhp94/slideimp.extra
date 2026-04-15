8bb528d7bd692693199c59cb0ba162a77164b935
hhp94 <58577711+hhp94@users.noreply.github.com>
2026-04-14 22:18:19 -0400

    remove chr M and 0

--- data-raw/msa_cache.R
+++ data-raw/msa_cache.R
@@ -2,6 +2,7 @@
 
 set_slideimp_path("dev")
 
-msa_cache <- ilmn_manifest("MSA")$feature
+msa_cache <- ilmn_manifest("MSA")
+msa_cache <- msa_cache[!group %in% c("M", "0"), ]
 
-saveRDS(msa_cache, here::here("inst/extdata/msa_cache.rds"), compress = "xz")
+saveRDS(msa_cache$feature, here::here("inst/extdata/msa_cache.rds"), compress = "xz")
